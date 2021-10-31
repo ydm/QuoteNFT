e5ab89bec6648e44fb80cbca1dcd0c6a258aaefd
Yordan Miladinov <jordanmiladinov@gmail.com>
2021-10-31 03:27:34 +0300

    quote nft is now linted

--- contracts/QuoteNFT.sol
+++ contracts/QuoteNFT.sol
@@ -4,26 +4,55 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract QuoteNFT is ERC721 {
+interface IQuoteNFT is IERC721 {
+    function mint(address to, string memory quote)
+        external
+        returns (uint256 tokenId);
+
+    function exists(uint256 tokenId) external view returns (bool);
+
+    function quote(uint256 tokenId) external view returns (string memory);
+}
+
+contract QuoteNFT is ERC721, IQuoteNFT {
     using Counters for Counters.Counter;
     Counters.Counter private _counter;
 
-    address public _owner = msg.sender;
-
-    constructor() ERC721("Quote NFT", "QFT") {}
+    address public owner;
+    mapping(uint256 => string) private _quotes;
 
     modifier restricted() {
-        require(msg.sender == _owner,
-                "This function is restricted to the contract's owner");
+        require(msg.sender == owner, "Function is restricted");
         _;
     }
 
-    function mint(address to) external restricted returns (uint256 tokenId) {
+    constructor() ERC721("Quote NFT", "QFT") {
+        owner = msg.sender;
+    }
+
+    function mint(address to, string memory quote_)
+        external
+        override
+        restricted
+        returns (uint256 tokenId)
+    {
         _counter.increment();
-
         uint256 id = _counter.current();
+        _quotes[id] = quote_;
         _mint(to, id);
-
         return id;
+    }
+
+    function exists(uint256 tokenId) external view override returns (bool) {
+        return _exists(tokenId);
+    }
+
+    function quote(uint256 tokenId)
+        external
+        view
+        override
+        returns (string memory)
+    {
+        return _quotes[tokenId];
     }
 }
